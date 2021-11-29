59e76192ad4ec5b53d95bfcc8320d84ca1394dc1
anjali kolhe <anjalikolhe7@gmail.com>
2021-11-29 11:34:32 +0530

    Packagin class structured

--- BusinessRulesEngine/src/main/java/processor/PackingSlipGenerator.java
+++ BusinessRulesEngine/src/main/java/processor/PackingSlipGenerator.java
@@ -1,4 +1,31 @@
 package processor;
 
+import constants.ProcessType;
+
 public class PackingSlipGenerator {
+
+    private ProcessType processType;
+    private Double payAmount;
+
+    public PackingSlipGenerator(ProcessType processType, Double payAmount) {
+        this.processType = processType;
+        this.payAmount = payAmount;
+    }
+
+    public ProcessType getProcessType() {
+        return processType;
+    }
+
+    public void setProcessType(ProcessType processType) {
+        this.processType = processType;
+    }
+
+    public Double getPayAmount() {
+        return payAmount;
+    }
+
+    public void setPayAmount(Double payAmount) {
+        this.payAmount = payAmount;
+    }
+
 }
