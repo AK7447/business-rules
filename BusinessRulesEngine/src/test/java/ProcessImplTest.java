c1299ba11fe9f69369541cacbfd051cc0c312fa8
anjali kolhe <anjalikolhe7@gmail.com>
2021-11-29 11:42:15 +0530

    Test case created for doPayment method

---
+++ BusinessRulesEngine/src/test/java/ProcessImplTest.java
@@ -0,0 +1,25 @@
+import orderprocessing.constants.ProcessType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProcessImplTest {
+
+    @InjectMocks
+    ProcessImpl process;
+
+    @Test
+    public void doPaymentTestBook() {
+        Double payAmount = 123.0;
+        process.doPayment(ProcessType.BOOK, 123.0);
+    }
+
+    public void doPaymentTestProduct() {
+        Double payAmount = 123.0;
+        process.doPayment(ProcessType.PHYSICAL_PRODUCT, 123.0);
+    }
+
+
+}
