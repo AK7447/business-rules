da78312f7d1b7919cb70383d08b8cf7e249527de
anjali kolhe <anjalikolhe7@gmail.com>
2021-11-29 11:36:08 +0530

    Generate packagin methods added

--- BusinessRulesEngine/src/main/java/processor/PackingSlipGenerator.java
+++ BusinessRulesEngine/src/main/java/processor/PackingSlipGenerator.java
@@ -1,5 +1,6 @@
 package processor;
 
+import constants.Department;
 import constants.ProcessType;
 
 public class PackingSlipGenerator {
@@ -26,6 +27,21 @@ public class PackingSlipGenerator {
 
     public void setPayAmount(Double payAmount) {
         this.payAmount = payAmount;
+    }
+
+
+    public void generatePackingSlip(Department department) {
+        String packingSlip = " Packing slip for " + department+ " of :" + this.processType + " with payment " + this.payAmount;
+        System.out.println(packingSlip);
+    }
+
+    public void generateCommissionPay(ProcessType processType) {
+        String commissionPay = "Commission pay has been generated for product type : " + processType;
+        System.out.println(commissionPay);
+    }
+
+    public void addFreeVideo() {
+        System.out.println("Free first AID has been added ");
     }
 
 }
